b2a0ab90cb7e65cb4b54fb7a9d46bfac32c2641a
Adam Gausmann <agausmann@fastmail.com>
2022-04-08 19:50:44 -0500

    Copy over main loop from another project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,161 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use pollster::block_on;
+use std::sync::Arc;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+pub type GraphicsContext = Arc<GraphicsContextInner>;
+
+pub struct GraphicsContextInner {
+    pub window: Window,
+    pub surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub render_format: wgpu::TextureFormat,
+}
+
+impl GraphicsContextInner {
+    async fn new(window: Window) -> anyhow::Result<Self> {
+        let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+        let surface = unsafe { instance.create_surface(&window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                ..Default::default()
+            })
+            .await
+            .context("failed to create adapter")?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await?;
+
+        let render_format = surface
+            .get_preferred_format(&adapter)
+            .context("failed to select a render format")?;
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            render_format,
+        })
+    }
+
+    fn reconfigure(&self) {
+        self.surface.configure(
+            &self.device,
+            &wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: self.render_format,
+                width: self.window.inner_size().width,
+                height: self.window.inner_size().height,
+                present_mode: wgpu::PresentMode::Fifo,
+            },
+        );
+    }
+}
+
+struct App {
+    gfx: GraphicsContext,
+}
+
+impl App {
+    async fn new(window: Window) -> anyhow::Result<Self> {
+        let gfx = Arc::new(GraphicsContextInner::new(window).await?);
+        gfx.reconfigure();
+
+        Ok(Self { gfx })
+    }
+
+    fn update(&mut self) {}
+
+    fn redraw(&mut self) -> anyhow::Result<()> {
+        let frame = loop {
+            match self.gfx.surface.get_current_texture() {
+                Ok(frame) => {
+                    if frame.suboptimal {
+                        self.gfx.reconfigure();
+                    } else {
+                        break frame;
+                    }
+                }
+                Err(wgpu::SurfaceError::Lost) => {
+                    self.gfx.reconfigure();
+                }
+                Err(wgpu::SurfaceError::Timeout) | Err(wgpu::SurfaceError::Outdated) => {
+                    return Ok(());
+                }
+                Err(err) => {
+                    return Err(err.into());
+                }
+            }
+        };
+
+        let frame_view = frame.texture.create_view(&Default::default());
+        let mut encoder = self.gfx.device.create_command_encoder(&Default::default());
+
+        {
+            let _pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &frame_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.gfx.queue.submit([encoder.finish()]);
+        frame.present();
+
+        Ok(())
+    }
+
+    fn window_resized(&mut self) {
+        self.gfx.reconfigure();
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(1280, 720))
+        .with_title("Exspherement")
+        .build(&event_loop)?;
+
+    let mut app = block_on(App::new(window))?;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::RedrawRequested(..) => {
+            app.update();
+            app.redraw().unwrap();
+        }
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => {
+                *control_flow = ControlFlow::Exit;
+            }
+            WindowEvent::Resized(..) | WindowEvent::ScaleFactorChanged { .. } => {
+                app.window_resized();
+            }
+            _ => {}
+        },
+        _ => {}
+    })
 }
