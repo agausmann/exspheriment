16f4db1b348930eeb6e395260849b386a2f64cb8
Adam Gausmann <agausmann@fastmail.com>
2022-06-13 19:58:19 -0500

    Add SimTimer and sim_tick

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,42 @@ pub mod math;
 pub mod orbit;
 pub mod time;
 
-use bevy::{app::App, DefaultPlugins};
+use std::time::Duration;
+
+use bevy::{
+    app::App,
+    core::FixedTimestep,
+    prelude::{ResMut, SystemSet},
+    DefaultPlugins,
+};
+use time::SimInstant;
+
+const SIM_INTERVAL: Duration = Duration::from_millis(10);
+
+struct SimTimer {
+    now: SimInstant,
+}
+
+impl SimTimer {
+    fn new() -> Self {
+        Self {
+            now: SimInstant::epoch(),
+        }
+    }
+}
+
+fn sim_tick(mut timer: ResMut<SimTimer>) {
+    timer.now += SIM_INTERVAL.into();
+}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .insert_resource(SimTimer::new())
+        .add_system_set(
+            SystemSet::new()
+                .with_system(sim_tick)
+                .with_run_criteria(FixedTimestep::step(SIM_INTERVAL.as_secs_f64())),
+        )
+        .add_plugins(DefaultPlugins)
+        .run()
 }
